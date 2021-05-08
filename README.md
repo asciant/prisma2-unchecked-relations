0ff932ac133856a0fc0140117697f77d83fa9f44
Haiden Taylor <haiden@asciant.com>
2021-05-09 07:39:38 +1000

    Working example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # prisma2-unchecked-relations
+
+## Actions Undertaken
+
+#### Working Example
+
+1. `npm install`
+2. Copy the example Schema from the [Prisma Relations Documentation](https://www.prisma.io/docs/concepts/components/prisma-schema/relations#types-of-relations) into `schema.prisma`
+3. I had to reset (delete the tables from the Quickstart database) and then run `npx prisma db push`
+4. I confirmed the tables were setup with the TablePlus App (because prisma studio didn't work).
+5. Run `npm run dev`
+6. Check the database, category and post should be connected
--- script.js
+++ script.js
@@ -4,7 +4,52 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+
+    // Create the user
+    const newUser = await prisma.user.create({
+    data: {},
+    })
+
+    // Create a post
+    const newPost = await prisma.post.create({
+        data: {
+            author: {
+                connect: {
+                    id: newUser.id
+                }
+            }
+        },
+    })
+
+    // Create a Category
+    const newCategory = await prisma.category.create({
+        data: {},
+    })
+
+    // // Connect Post to a Category
+    // const connectedPost = await prisma.post.update({
+    //     where: { id: newPost.id },
+    //     data: {
+    //         categories: {
+    //             connect: {
+    //                 id: newCategory.id
+    //             }
+    //         }
+    //      }
+    // })
+
+    // Connect Category to a Post
+    const connectedCategory = await prisma.category.update({
+        where: { id: newCategory.id },
+        data: {
+            posts: {
+                connect: {
+                    id: newPost.id
+                }
+            }
+         }
+    })
+
 }
 
 main()
